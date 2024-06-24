edd69a577b8b6582c87f25abe1b3b92ab03aac33
skinnynoonie <skinnynoonie@users.noreply.github.com>
2024-06-24 13:55:02 -0400

    Created a roman numeral converter util class

--- src/main/java/me/skinnynoonie/util/check/Arguments.java
+++ src/main/java/me/skinnynoonie/util/check/Arguments.java
@@ -8,6 +8,19 @@ public final class Arguments {
         }
     }
 
+    public static void inRange(double number, double min, double max, String argName) {
+        if (number < min || number > max) {
+            throw new IllegalArgumentException
+                    ("Argument " + argName + " is not in range " + min + " to " + max + " (inclusive)");
+        }
+    }
+
+    public static void legal(boolean condition, String exceptionMessage, Object... arguments) {
+        if (!condition) {
+            throw new IllegalArgumentException(exceptionMessage.formatted(arguments));
+        }
+    }
+
     private Arguments() {
         throw new UnsupportedOperationException("can not initialize util class");
     }
---
+++ src/main/java/me/skinnynoonie/util/number/roman/RomanNumeralConverter.java
@@ -0,0 +1,70 @@
+package me.skinnynoonie.util.number.roman;
+
+import me.skinnynoonie.util.check.Arguments;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+import java.util.Map;
+
+public final class RomanNumeralConverter {
+
+    private static final String[] NUMERALS    = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    private static final int[] NUMERAL_VALUES = { 1000,900,  500, 400,  100, 90,   50,  40,   10,  9,    5,   4,    1 };
+    private static final Map<Character, Integer> SIG_NUMERALS_MAP = Map.of(
+            'M', 1000,
+            'D', 500,
+            'C', 100,
+            'L', 50,
+            'X', 10,
+            'V', 5,
+            'I', 1
+    );
+
+    public static @NotNull String toNumerals(@Range(from = 1, to = 3999) int number) {
+        Arguments.inRange(number, 1, 3999, "number");
+
+        StringBuilder numeralBuilder = new StringBuilder();
+        for (int i = 0; i < NUMERALS.length; i++) {
+            while (number - NUMERAL_VALUES[i] >= 0) {
+                number = number - NUMERAL_VALUES[i];
+                numeralBuilder.append(NUMERALS[i]);
+            }
+        }
+
+        return numeralBuilder.toString();
+    }
+
+    public static @Range(from = 1, to = 3999) int parseValue(@NotNull String numerals) {
+        Arguments.notNull(numerals, "numerals");
+
+        int value = 0;
+        for (int i = 0; i < numerals.length(); i++) {
+            int currentNumeralValue = findNumeralValueOrThrow(numerals.charAt(i), i, numerals);
+            if (i + 1 < numerals.length()) {
+                int nextNumeralValue = findNumeralValueOrThrow(numerals.charAt(i + 1), i + 1, numerals);
+                if (currentNumeralValue < nextNumeralValue) {
+                    value += nextNumeralValue - currentNumeralValue;
+                    i++;
+                } else {
+                    value += currentNumeralValue;
+                }
+            } else {
+                value += currentNumeralValue;
+            }
+        }
+
+        return value;
+    }
+
+    private static int findNumeralValueOrThrow(char numeral, int index, String input) {
+        Integer numeralValue = SIG_NUMERALS_MAP.get(numeral);
+        Arguments.legal(numeralValue != null,
+                "illegal numeral '%s' found at index %s from input '%s'", numeral, index, input);
+        return numeralValue;
+    }
+
+    private RomanNumeralConverter() {
+        throw new UnsupportedOperationException("can not instantiate util class");
+    }
+
+}
