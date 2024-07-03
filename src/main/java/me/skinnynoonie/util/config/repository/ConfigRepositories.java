2d847206c513aa0b1fdf402d737092bcb9f30b6f
skinnynoonie <skinnynoonie@users.noreply.github.com>
2024-07-03 13:23:15 -0400

    no more yaml (all apis suck)

--- src/main/java/me/skinnynoonie/util/config/repository/ConfigRepositories.java
+++ src/main/java/me/skinnynoonie/util/config/repository/ConfigRepositories.java
@@ -1,7 +1,6 @@
 package me.skinnynoonie.util.config.repository;
 
 import me.skinnynoonie.util.config.serialize.JsonConfigSerializer;
-import me.skinnynoonie.util.config.serialize.YmlConfigSerializer;
 import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Path;
@@ -10,10 +9,6 @@ public final class ConfigRepositories {
 
     public static @NotNull ConfigRepository concurrentLocalJson(@NotNull Path configFolder) {
         return new ConcurrentLocalFileConfigRepository(configFolder, JsonConfigSerializer.createDefault());
-    }
-
-    public static @NotNull ConfigRepository concurrentLocalYml(@NotNull Path configFolder) {
-        return new ConcurrentLocalFileConfigRepository(configFolder, YmlConfigSerializer.createDefault());
     }
 
     private ConfigRepositories() {
